da9b3ece2611b1808757e5c447b431d472706417
LazezBZH <lazez.bzh@caramail.fr>
2024-01-16 12:52:36 +0100

    slider

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,6 @@ const nav = document.querySelector("nav");
 window.addEventListener("scroll", fixeNav);
 function fixeNav() {
   if (window.scrollY >= screen.height * 0.3) {
-    console.log("coucu");
     nav.style.position = "fixed";
     nav.style.top = "0";
   } else {
